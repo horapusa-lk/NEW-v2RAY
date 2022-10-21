388be9e70536cee017c733c24cd48e55842bf0d4
Bhathiya Gamage <73831309+horapusa-lk@users.noreply.github.com>
2022-10-21 13:57:21 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
+apt update
+apt upgrade -y
 apt -y install python3.10 python3.10-venv python3-pip
-
+bash v2ray.sh
 pip3 install -r requirements.txt
 mkdir /root/bot
 cp bot.py /root/bot
 chmod +x /root/bot/bot.sh
 cp horapusa.service /etc/systemd/system/
 sudo systemctl enable horapusa.service
-bash v2ray.sh
+sudo systemctl start horapusa.service
