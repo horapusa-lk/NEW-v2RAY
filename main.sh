512a567d2dd658f0205c3c9a50d553a2da4fb84e
Bhathiya Gamage <73831309+horapusa-lk@users.noreply.github.com>
2022-10-21 13:56:00 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,9 @@
-apt install speedtest-cli -y
-apt install python3
-apt install python3-pip
-pip3 install python-telegram-bot requests
+apt -y install python3.10 python3.10-venv python3-pip
+
+pip3 install -r requirements.txt
 mkdir /root/bot
 cp bot.py /root/bot
 chmod +x /root/bot/bot.sh
 cp horapusa.service /etc/systemd/system/
 sudo systemctl enable horapusa.service
-cp bot.token /root/bot
-pip3 install psutil
-python3 vless.py
-reboot
+bash v2ray.sh
